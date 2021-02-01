b23f7a1b0e5f2262957ec0b953fbd734d50f19a6
EvgenDEP1 <jen_dostovalov@mail.ru>
2021-02-01 10:01:53 +0500

    HomeWork

--- 210525/dr_3.py
+++ 210525/dr_3.py
@@ -1,17 +1,25 @@
 import re
 
 
-RE_NUMBERS_VALIDATOR = re.compile(r'^[0-9]+[.,][0-9]+$')
+# RE_NUMBERS_VALIDATOR = re.compile(r'^[0-9]+[.,][0-9]+$')
+
+
+# изменил на паре
+RE_NUMBERS_VALIDATOR = re.compile(r'^[0-9]+[.,]?([0-9]+)?$')
 
 
 def numbers_is_valid(numbers):
     return RE_NUMBERS_VALIDATOR.match(numbers)
 
 
-assert numbers_is_valid('1.32')
+assert numbers_is_valid('1')
+assert numbers_is_valid('132')
 assert numbers_is_valid('1,32')
+assert not numbers_is_valid('1,32,32')
 assert not numbers_is_valid(',')
 
-print(numbers_is_valid('1.32'))
+print(numbers_is_valid('1'))
+print(numbers_is_valid('132'))
 print(numbers_is_valid('1,32'))
 print(numbers_is_valid(','))
+print(numbers_is_valid('1,32,32'))
