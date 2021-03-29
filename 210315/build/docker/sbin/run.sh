1132a50bacdab605cb9bbb034a14c20dce5f82cb
EvgenDEP1 <jen_dostovalov@mail.ru>
2021-03-29 10:28:55 +0500

    hw

--- 210315/build/docker/sbin/run.sh
+++ 210315/build/docker/sbin/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "Starting time_messenger docker"
-python3 "$KPK_BIN/iTTime_messenger/manage.py" runserver 0.0.0.0:8000
+python3 "$KPK_BIN/time_messenger/manage.py" runserver 0.0.0.0:8000
--- 210315/sources/iTTime_messenger/mainapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from mainapp.models import Dialog, DialogMemebers, Message
-
-admin.site.register(Dialog)
-admin.site.register(DialogMemebers)
-admin.site.register(Message)
-
-# Register your models here.
